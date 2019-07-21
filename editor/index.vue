a0ed7e3f445c13df8def4ffddc776a84c97137f8
liulei <lei.liul@ymm56.com>
2019-07-21 19:46:51 +0800

    Initial commit

---
+++ editor/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="component-editor">
+    <el-form size="mini" label-width="100px">
+      <el-form-item label="选择种类:">
+        <div class="sty">
+          <div class="imgSty" v-for="i in titleStyleMap" :key="i.type" @click="chooseType(i.type)">
+            <img :src="i.img" alt="">
+          </div>
+        </div>
+      </el-form-item>
+      <el-form-item label="引言内容:">
+        <el-input type="input" v-model="componentInfo.content" placeholder="请输入标题名称"></el-input>
+      </el-form-item>
+      <el-form-item label="文字大小:">
+        <el-input placeholder="请输入文字大小" v-model="componentInfo.font" min='0' type='number'>
+          <template slot="append">px</template>
+        </el-input>
+      </el-form-item>
+      <el-form-item label="文字颜色:">
+        <el-color-picker v-model="componentInfo.fontC" show-alpha></el-color-picker>
+      </el-form-item>
+      <el-form-item label="背景颜色:" v-if="num == 1">
+        <el-color-picker v-model="componentInfo.baColor" show-alpha></el-color-picker>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'maliangeditor',
+    props: {
+      componentInfo: { // 固定字段，收集所有属性值
+        type: [Object],
+        default () {
+          return {
+            type: 2,
+            content: ''
+          }
+        }
+      }
+    },
+    data: function () {
+      return {
+        num: 1,
+        titleStyleMap: [
+          {
+            type: 1,
+            img: 'https://gw.alipayobjects.com/os/q/cms/images/jnlh8d66/e28ad88e-48a1-4c2a-af63-d24f3443d8cd.cms/images/fff86f5d-e206-4ffb-8d8a-a8df90207242.png?x-oss-process=image/resize,w_560/crop,h_560'
+          },{
+            type: 2,
+            img: 'https://gw.alipayobjects.com/os/q/cms/images/jnlh85i0/fb89e9e1-9bbf-4052-9f01-ebab6e6cdf77.cms/images/96c50b7a-fb2b-4950-bcdc-8aca26498319.png?x-oss-process=image/resize,w_560/crop,h_560'
+          },{
+            type: 3,
+            img: 'https://gw.alipayobjects.com/os/q/cms/images/jnlh4gn0/c6a4c09d-5820-45bd-847e-80ed80157070.cms/images/997a825b-28ad-493b-ad2c-6829e0f08711.png?x-oss-process=image/resize,w_560/crop,h_560'
+          },{
+            type: 4,
+            img: 'https://gw.alipayobjects.com/os/q/cms/images/jnlh4b9s/61a76512-cfab-4af4-8999-4de4b8d80ecb.cms/images/a9e8f44e-20a5-45f4-9a9b-994a94c21f1a.png?x-oss-process=image/resize,w_560/crop,h_560'
+          },{
+            type: 5,
+            img: 'https://gw.alipayobjects.com/os/q/cms/images/jnlh46ci/464c75d9-8c5b-4fad-ab2b-b5868d7237d2.cms/images/da4fca2a-046d-43e3-a669-cc653b2d8dec.png?x-oss-process=image/resize,w_560/crop,h_560'
+          }
+        ]
+      }
+    },
+    computed: {
+    },
+    watch: {
+      componentInfo: {
+        hanlder: function (v) {
+          console.log('editor index', v)
+        },
+      }
+    },
+    mounted: function () {
+    },
+    methods: {
+      chooseType (type) {
+        this.num = type
+        this.componentInfo.type = type
+        this.componentInfo.baColor = type == 1 ? '#f7f7f7' : '#fff'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
+  .component-editor {
+    .sty{
+      // border 1px solid #ddd
+      height 250px
+      overflow hidden
+      overflow-y auto
+      .imgSty {
+        width 400px
+        height 80px
+        cursor pointer
+        margin-bottom 10px
+        img {
+          height 100%
+        }
+      }
+    }
+  }
+</style>
---
+++ src/index.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="component">
+    <div class="comp">
+      <div class="fd-foreword" :class="{fd_border: type == 5}">
+        <div class="fd-foreword-left">
+          <img :src="iconType" alt="" :class="{imgType5: type == 5}">
+        </div>
+        <div class="comp_quote_1-left-line" v-if="type == 2"></div>
+        <div class="fd-foreword-body">
+          <div :class="classObject" class="text" :style="{backgroundColor: baColor,fontSize: font + 'px',color: fontC}">{{content}}</div>
+        </div>
+        <div class="fd-foreword-right" v-if="type == 4">
+          <img src="./assets/icon4_bottom.png" alt="" class="">
+        </div>
+      </div> 
+    </div>
+  </div>
+</template>
+
+<script>
+  import {VueExtend} from 'godspen-lib'
+
+  export default {
+    mixins: [VueExtend.mixin],
+    name: 'introduction',
+    label: process.env.LABEL,
+    style: process.env.STYLE,
+    props: {
+      type: {
+        type: Number,
+        default: 1,
+        editer: {
+          ignore: true // 忽略，码良基础在编辑器中不显示
+        }
+      },
+      content: {
+        type: String,
+        default: '云凤蝶的文化：客户第一，追求极致，勇于创新，开放共赢 / 云凤蝶的品牌定位：丰富、简单、个性、开放。',
+        editer: {
+          ignore: true // 忽略，码良基础在编辑器中不显示
+        }
+      },
+      baColor: {
+        type: String,
+        default: '',
+        editer: {
+          ignore: true // 忽略，码良基础在编辑器中不显示
+        }
+      },
+      font: {
+        type: String,
+        default: '14',
+        editer: {
+          ignore: true // 忽略，码良基础在编辑器中不显示
+        }
+      },
+      fontC: {
+        type: String,
+        default: '#999',
+        editer: {
+          ignore: true // 忽略，码良基础在编辑器中不显示
+        }
+      },
+    },
+    computed: {
+      iconType () {
+        return {
+          1: require('./assets/icon1.png'),
+          2: require('./assets/icon2.png'),
+          3: require('./assets/icon3.png'),
+          4: require('./assets/icon4_top.png'),
+          5: require('./assets/icon5.png'),
+        }[this.type]
+      },
+      classObject () {
+        return {
+          1: 'text1',
+          2: 'text2',
+          3: 'text3',
+          4: 'text4',
+          5: 'text5',
+          6: 'text6',
+        }[this.type]
+      }
+    },
+    editorMethods: {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
+  .component {
+    width 100%
+    .comp {
+      padding 8px 16px
+      .fd_border {
+        border: 1px solid #000;
+      }
+      .fd-foreword {
+        padding 0 0 0 36px
+        position relative
+        .fd-foreword-left {
+          position absolute
+          left 0
+          right 0
+          img {
+            width 18px
+            height 18px
+          }
+        }
+        .fd-foreword-right {
+          position absolute
+          bottom 0
+          right 0
+          img {
+            width 18px
+            height 18px
+          }
+        }
+        .comp_quote_1-left-line {
+          position: absolute;
+          width: 3px;
+          top: 24px;
+          left 10px
+          bottom: 0;
+          transform: translateX(-50%);
+          background: #e9e9e9;
+        }
+        .text {
+          font-size: 14px;
+        }
+        .text1 {
+          display: block;
+          padding: 8px 12px;
+          color: #999;
+          background: #f7f7f7;
+        }
+        .text2, .text3 {
+          color: #999;
+        }
+        .text4 {
+          color: #333;
+        }
+        .text5 {
+          color: #333;
+          padding 8px 0
+        }
+        .text6 {
+
+        }
+        .imgType5 {
+          background-color #000
+        }
+      }
+    }
+  }
+</style>
